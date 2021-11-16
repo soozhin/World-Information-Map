2cf230fb2ba622d9d74a0bb520ee2cee4b8488c4
soozhin <53286620+soozhin@users.noreply.github.com>
2021-11-16 18:46:29 +0900

    世界地図を真ん中に置くことに成功した

--- PBLproject/worldmap/static/javascript/start.js
+++ PBLproject/worldmap/static/javascript/start.js
@@ -1,3 +1,13 @@
-var country_population = document.getElementsById("country-population")
-var t = document.createTextNode("国と人口2");
-country_population.appendChild(t);
+// var country_population = document.getElementById("country-population");
+// var land = document.getElementsByClassName("land")
+
+// country_population.addEventListener("mouseover", mouseOver);
+// country_population.addEventListener("mouseout", mouseOut);
+
+// function mouseOver() {
+//     country_population.innerHTML = "2";
+// }
+
+// function mouseOut() {
+//     country_population.innerHTML = "4";
+// }
