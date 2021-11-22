289304d5050311e59380f6c79d09388cb9ce5cf9
soozhin <53286620+soozhin@users.noreply.github.com>
2021-11-22 17:06:49 +0900

    なぜかstaticファイルが正しく読み込めないため，start.cssとstart.jsを一個上のディレクトリに持ってきた

---
+++ PBLproject/worldmap/static/start.js
@@ -0,0 +1,42 @@
+var country_population = document.getElementById("country-population");
+var country_id = [];
+var country = [];
+var all_country = document.getElementsByTagName("path");
+var default_country_population = "";
+var container = document.getElementsByClassName("container");
+
+// 表示のところに初期化する
+country_population.innerHTML = default_country_population;
+
+// 新しい「label」を作り，国の名前を入れる
+const country_population_textbox = document.createElement("label");
+country_population_textbox.setAttribute("for", "country-population-textbox");
+country_population_textbox.style.position = "absolute";
+country_population_textbox.style.background = "red";
+
+// 世界地図にあるすべての国のidタグを保存しておく
+// そして，idによって各国のタグを保存しておく
+// それぞれの国にmouseoverが発生したら，自分の国名を表示させて，
+// ポインターが出たら(mouseout)，初期値に戻す
+// 「label」にマウスの位置を提供して，マウスの少し上のところに表示させる
+for (var i = 0; i < all_country.length; i++) {
+    country_id[i] = all_country[i].getAttribute("id");
+    country[i] = document.getElementById(country_id[i])
+    country[i].addEventListener("mouseover", showCountryPopulation);
+    country[i].addEventListener("mouseout", showDefaultCountryPopulation);
+}
+
+function showCountryPopulation(event) {
+    country_population.innerHTML = this.getAttribute("title");
+    country_population_textbox.innerHTML = this.getAttribute("title");
+    var mouse_x = event.clientX;
+    var mouse_y = event.clientY;
+    country_population_textbox.style.top = mouse_y - 25;
+    country_population_textbox.style.left = mouse_x - 10;
+    country_population_textbox.style.zIndex = 1;
+    country_population.appendChild(country_population_textbox);
+}
+
+function showDefaultCountryPopulation() {
+    country_population.innerHTML = default_country_population;
+}
