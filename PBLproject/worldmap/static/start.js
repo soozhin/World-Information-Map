388f3b6e386d64f62c6d8e523ee37fb7a6801788
soozhin <53286620+soozhin@users.noreply.github.com>
2021-11-22 18:24:19 +0900

    テキストボックスを真ん中に表示させた．そして，テキストボックスが画面の外に出たら，ポインタの下に表示させるよ下に表示させるようにした

--- PBLproject/worldmap/static/start.js
+++ PBLproject/worldmap/static/start.js
@@ -1,12 +1,15 @@
-var country_population = document.getElementById("country-population");
 var country_id = [];
 var country = [];
 var all_country = document.getElementsByTagName("path");
-var default_country_population = "";
 var container = document.getElementsByClassName("container");
 
-// 表示のところに初期化する
-country_population.innerHTML = default_country_population;
+// https://jsfiddle.net/knLbjc9a/4/
+// 上記のサイトからコピペしたコード
+// svgの空白のスペースを切り取ってくれる
+var svg = document.getElementsByTagName("svg")[0];
+var bbox = svg.getBBox();
+var viewBox = [bbox.x, bbox.y, bbox.width, bbox.height].join(" ");
+svg.setAttribute("viewBox", viewBox);
 
 // 新しい「label」を作り，国の名前を入れる
 const country_population_textbox = document.createElement("label");
@@ -18,25 +21,30 @@ country_population_textbox.style.background = "red";
 // そして，idによって各国のタグを保存しておく
 // それぞれの国にmouseoverが発生したら，自分の国名を表示させて，
 // ポインターが出たら(mouseout)，初期値に戻す
-// 「label」にマウスの位置を提供して，マウスの少し上のところに表示させる
+// 「label」をマウスの少し上のところに表示させる
+// たまに「label」が上に行きすぎると，下に表示させる
 for (var i = 0; i < all_country.length; i++) {
     country_id[i] = all_country[i].getAttribute("id");
     country[i] = document.getElementById(country_id[i])
     country[i].addEventListener("mouseover", showCountryPopulation);
-    country[i].addEventListener("mouseout", showDefaultCountryPopulation);
+    country[i].addEventListener("mouseout", hideCountryPopulation);
 }
 
 function showCountryPopulation(event) {
-    country_population.innerHTML = this.getAttribute("title");
     country_population_textbox.innerHTML = this.getAttribute("title");
     var mouse_x = event.clientX;
     var mouse_y = event.clientY;
-    country_population_textbox.style.top = mouse_y - 25;
-    country_population_textbox.style.left = mouse_x - 10;
+    var textbox_x = mouse_x - country_population_textbox.clientWidth / 2 + window.pageXOffset;
+    var textbox_y = mouse_y - 30 + window.pageYOffset;
+    if (window.pageYOffset + 10 > textbox_y) {
+        textbox_y = mouse_y + 10 + window.pageYOffset;
+    }
+    country_population_textbox.style.top = textbox_y;
+    country_population_textbox.style.left = textbox_x;
     country_population_textbox.style.zIndex = 1;
-    country_population.appendChild(country_population_textbox);
+    document.body.appendChild(country_population_textbox);
 }
 
-function showDefaultCountryPopulation() {
-    country_population.innerHTML = default_country_population;
+function hideCountryPopulation(event) {
+    country_population_textbox.innerHTML = "";
 }
