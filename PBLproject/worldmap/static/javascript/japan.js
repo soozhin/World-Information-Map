659cd69832c896df44f736139600692874f1a14f
Nephy38 <19026227@mmm.muroran-it.ac.jp>
2021-12-06 11:57:27 +0900

    日本と都道府県を世界地図のテキストボックスに合わせた

--- PBLproject/worldmap/static/javascript/japan.js
+++ PBLproject/worldmap/static/javascript/japan.js
@@ -1,43 +1,51 @@
-var todouhuken_population = document.getElementById("todouhuken-population");
 var todouhuken_id = [];
 var todouhuken = [];
 var all_todouhuken = document.getElementsByTagName("path");
-var default_todouhuken_population = "todouhuken name and population";
 var container = document.getElementsByClassName("container");
 
-// 表示のところに初期化する
-todouhuken_population.innerHTML = default_todouhuken_population;
+// https://jsfiddle.net/knLbjc9a/4/
+// 上記のサイトからコピペしたコード
+// svgの空白のスペースを切り取ってくれる
+var svg = document.getElementsByTagName("svg")[0];
+var bbox = svg.getBBox();
+var viewBox = [bbox.x, bbox.y, bbox.width, bbox.height].join(" ");
+svg.setAttribute("viewBox", viewBox);
 
-// 新しい「label」を作り，都道府県の名前を入れる
+// 新しい「label」を作り，国の名前を入れる
 const todouhuken_population_textbox = document.createElement("label");
 todouhuken_population_textbox.setAttribute("for", "todouhuken-population-textbox");
 todouhuken_population_textbox.style.position = "absolute";
 todouhuken_population_textbox.style.background = "red";
 
-// 日本地図にあるすべての都道府県のidタグを保存しておく
-// そして，idによって各都道府県のタグを保存しておく
-// それぞれの都道府県にmouseoverが発生したら，自分の都道府県名を表示させて，
+// 世界地図にあるすべての国のidタグを保存しておく
+// そして，idによって各国のタグを保存しておく
+// それぞれの国にmouseoverが発生したら，自分の国名を表示させて，
 // ポインターが出たら(mouseout)，初期値に戻す
-// 「label」にマウスの位置を提供して，マウスの少し上のところに表示させる
+// 「label」をマウスの少し上のところに表示させる
+// たまに「label」が上に行きすぎると，下に表示させる
 for (var i = 0; i < all_todouhuken.length; i++) {
     todouhuken_id[i] = all_todouhuken[i].getAttribute("id");
     todouhuken[i] = document.getElementById(todouhuken_id[i])
     todouhuken[i].addEventListener("mouseover", showtodouhukenPopulation);
-    todouhuken[i].addEventListener("mouseout", showDefaulttodouhukenPopulation);
+    todouhuken[i].addEventListener("mouseout", hidetodouhukenPopulation);
 }
 
 function showtodouhukenPopulation(event) {
-    todouhuken_population.innerHTML = this.getAttribute("title");
     todouhuken_population_textbox.innerHTML = this.getAttribute("title");
     var mouse_x = event.clientX;
     var mouse_y = event.clientY;
-    todouhuken_population_textbox.style.top = mouse_y - 25;
-    todouhuken_population_textbox.style.left = mouse_x - 10;
+    var textbox_x = mouse_x - todouhuken_population_textbox.clientWidth / 2 + window.pageXOffset;
+    var textbox_y = mouse_y - 30 + window.pageYOffset;
+    if (window.pageYOffset + 10 > textbox_y) {
+        textbox_y = mouse_y + 10 + window.pageYOffset;
+    }
+    todouhuken_population_textbox.style.top = textbox_y;
+    todouhuken_population_textbox.style.left = textbox_x;
     todouhuken_population_textbox.style.zIndex = 1;
-    todouhuken_population.appendChild(todouhuken_population_textbox);
+    document.body.appendChild(todouhuken_population_textbox);
 }
 
-function showDefaulttodouhukenPopulation() {
-    todouhuken_population.innerHTML = default_todouhuken_population;
-}
 
+function hidetodouhukenPopulation(event) {
+    todouhuken_population_textbox.innerHTML = "";
+}
--- PBLproject/worldmap/static/javascript/todouhuken.js
+++ PBLproject/worldmap/static/javascript/todouhuken.js
@@ -1,42 +1,51 @@
-var sikutyouson_population = document.getElementById("sikutyouson-population");
 var sikutyouson_id = [];
 var sikutyouson = [];
 var all_sikutyouson = document.getElementsByTagName("path");
-var default_sikutyouson_population = "sikutyouson name and population";
 var container = document.getElementsByClassName("container");
 
-// 表示のところに初期化する
-sikutyouson_population.innerHTML = default_sikutyouson_population;
+// https://jsfiddle.net/knLbjc9a/4/
+// 上記のサイトからコピペしたコード
+// svgの空白のスペースを切り取ってくれる
+var svg = document.getElementsByTagName("svg")[0];
+var bbox = svg.getBBox();
+var viewBox = [bbox.x, bbox.y, bbox.width, bbox.height].join(" ");
+svg.setAttribute("viewBox", viewBox);
 
-// 新しい「label」を作り，都道府県の名前を入れる
+// 新しい「label」を作り，国の名前を入れる
 const sikutyouson_population_textbox = document.createElement("label");
 sikutyouson_population_textbox.setAttribute("for", "sikutyouson-population-textbox");
 sikutyouson_population_textbox.style.position = "absolute";
 sikutyouson_population_textbox.style.background = "red";
 
-// 日本地図にあるすべての都道府県のidタグを保存しておく
-// そして，idによって各都道府県のタグを保存しておく
-// それぞれの都道府県にmouseoverが発生したら，自分の都道府県名を表示させて，
+// 世界地図にあるすべての国のidタグを保存しておく
+// そして，idによって各国のタグを保存しておく
+// それぞれの国にmouseoverが発生したら，自分の国名を表示させて，
 // ポインターが出たら(mouseout)，初期値に戻す
-// 「label」にマウスの位置を提供して，マウスの少し上のところに表示させる
+// 「label」をマウスの少し上のところに表示させる
+// たまに「label」が上に行きすぎると，下に表示させる
 for (var i = 0; i < all_sikutyouson.length; i++) {
     sikutyouson_id[i] = all_sikutyouson[i].getAttribute("id");
     sikutyouson[i] = document.getElementById(sikutyouson_id[i])
     sikutyouson[i].addEventListener("mouseover", showsikutyousonPopulation);
-    sikutyouson[i].addEventListener("mouseout", showDefaultsikutyousonPopulation);
+    sikutyouson[i].addEventListener("mouseout", hidesikutyousonPopulation);
 }
 
 function showsikutyousonPopulation(event) {
-    sikutyouson_population.innerHTML = this.getAttribute("title");
     sikutyouson_population_textbox.innerHTML = this.getAttribute("title");
     var mouse_x = event.clientX;
     var mouse_y = event.clientY;
-    sikutyouson_population_textbox.style.top = mouse_y - 25;
-    sikutyouson_population_textbox.style.left = mouse_x - 10;
+    var textbox_x = mouse_x - sikutyouson_population_textbox.clientWidth / 2 + window.pageXOffset;
+    var textbox_y = mouse_y - 30 + window.pageYOffset;
+    if (window.pageYOffset + 10 > textbox_y) {
+        textbox_y = mouse_y + 10 + window.pageYOffset;
+    }
+    sikutyouson_population_textbox.style.top = textbox_y;
+    sikutyouson_population_textbox.style.left = textbox_x;
     sikutyouson_population_textbox.style.zIndex = 1;
-    sikutyouson_population.appendChild(sikutyouson_population_textbox);
+    document.body.appendChild(sikutyouson_population_textbox);
 }
 
-function showDefaultsikutyousonPopulation() {
-    sikutyouson_population.innerHTML = default_sikutyouson_population;
+
+function hidesikutyousonPopulation(event) {
+    sikutyouson_population_textbox.innerHTML = "";
 }
